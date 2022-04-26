709ad5368a795b1828dfcb522091682f379a361e
Ross <rossgarfield@gmail.com>
2022-04-25 19:17:02 -0500

    commit tree 357abe0b6935af22515e93da6c78fc89f5c772b9

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const hq = 42;
+function distanceFromHqInBlocks(blockNum) {
+    return Math.abs(blockNum - hq);
+};
+
+function distanceFromHqInFeet(blockNum) {
+    return (264 * Math.abs(blockNum - hq));
+};
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+    return (264 * Math.abs(endBlock - startBlock));
+};
+
+function calculatesFarePrice(startBlock, endBlock) {
+    let distanceTravelled = (264 * Math.abs(endBlock - startBlock));
+    if (distanceTravelled < 400) {
+        return 0;
+    } else if (distanceTravelled > 400 && distanceTravelled <= 2000) {
+        return (distanceTravelled - 400) * .02;
+    } else if (distanceTravelled > 2000 && distanceTravelled <= 2500) {
+        return 25;
+    } else if (distanceTravelled > 2500) {
+        return 'cannot travel that far'
+    };
+};
